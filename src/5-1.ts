c237a2b56207dfa5b789000a7818aa74ac90e8f5
Fdebijl <18357629+Fdebijl@users.noreply.github.com>
2022-02-12 18:47:06 +0100

    Add day 5-2

--- src/5-1.ts
+++ src/5-1.ts
@@ -7,6 +7,21 @@ type Vector = {
   y2: number;
 }
 
+/** new Array(yMax).fill(0) makes copies by reference for some reason, so we need this */
+const generateGrid = (xMax: number, yMax: number): number[][] => {
+  const out = [];
+
+  for (let x = 0; x < xMax; x++) {
+    const row: number[] = [];
+    for (let y = 0; y < yMax; y++) {
+      row.push(0);
+    }
+    out.push(row)
+  }
+
+  return out;
+}
+
 const getStep = (from: number, to: number) => {
   if (from === to) return 0;
   return to - from > 0 ? 1 : -1
@@ -30,26 +45,30 @@ const inputToVectors = (input: string[], orthogonalOnly: boolean): Vector[] => {
   return output;
 }
 
-const input = getInput(5).split('\n');
-const vectors = inputToVectors(input, true);
-const yMax = Math.max(...vectors.map(vector => [vector.y1, vector.y2]).flat(2)) + 1;
-const xMax = Math.max(...vectors.map(vector => [vector.x1, vector.x2]).flat(2)) + 1;
-const grid: number[][] = new Array(xMax).fill(new Array(yMax).fill(0));
+export const getIntersectionCount = (orthogonalOnly: boolean) => {
+  const input = getInput(5).split('\n');
+  const vectors = inputToVectors(input, orthogonalOnly);
+  const yMax = Math.max(...vectors.map(vector => [vector.y1, vector.y2]).flat(2)) + 1;
+  const xMax = Math.max(...vectors.map(vector => [vector.x1, vector.x2]).flat(2)) + 1;
+  const grid: number[][] = generateGrid(xMax, yMax);
 
-for (const vector of vectors) {
-  let x = vector.x1;
-  let y = vector.y1;
-  const xStep = getStep(vector.x1, vector.x2);
-  const yStep = getStep(vector.y1, vector.y2);
+  for (const vector of vectors) {
+    let x = vector.x1;
+    let y = vector.y1;
+    const xStep = getStep(vector.x1, vector.x2);
+    const yStep = getStep(vector.y1, vector.y2);
 
-  while (!(x === vector.x2 && y === vector.y2)) {
+    while (!(x === vector.x2 && y === vector.y2)) {
+      grid[y][x]++;
+      y += yStep;
+      x += xStep;
+    }
+
     grid[y][x]++;
-    y += yStep;
-    x += xStep;
   }
+
+  const intersections = grid.flat(Infinity).filter(number => number > 1).length;
+  return intersections;
 }
 
-debugger;
-
-const intersections = grid.flat(2).filter(number => number > 1).length;
-console.log(intersections);
+console.log(getIntersectionCount(true));
---
+++ src/5-2.ts
@@ -0,0 +1,3 @@
+import { getIntersectionCount } from "./5-1";
+
+console.log(getIntersectionCount(false));
